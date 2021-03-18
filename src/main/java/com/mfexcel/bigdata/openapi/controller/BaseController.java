348ea0b404b40a8b9b3b4307fc6d0115cf12e815
wumutong <wumutong121@163.com>
2021-03-18 14:16:49 +0800

    增加插入/状态值/跨域 相关功能代码

--- src/main/java/com/mfexcel/bigdata/openapi/controller/BaseController.java
+++ src/main/java/com/mfexcel/bigdata/openapi/controller/BaseController.java
@@ -11,14 +11,13 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,10 +38,32 @@ public class BaseController {
     @Autowired
     private LoadMapperData loadMapperData;
 
-    @PostMapping
+
+//
+//    private void setCORS(HttpServletResponse resp,HttpServletRequest request){
+//        resp.setStatus(204);
+//        resp.setHeader("Access-Control-Allow-Origin", request.getHeader("http://10.0.60.12"));
+//        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+//        resp.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
+//        resp.setHeader("Access-Control-Allow-Credentials","true");
+//    }
+
+
+    //@PostMapping
+    @RequestMapping(method = {RequestMethod. POST ,RequestMethod.OPTIONS })
     public AjaxResult handler(HttpServletRequest request, @RequestBody Map<String, Object> paramMap) throws Throwable {
         //获取接口地址
         String uri = request.getRequestURI();
+//        String MethodName = request.getMethod();
+//
+//        setCORS(resp,request);
+//
+//        if(MethodName.equals("OPTIONS")){
+//            return AjaxResult.no_content(204);
+//        }
+//
+//    System.out.println("*****************"+MethodName+"******************");
+
         //读取XML配置文件
         SqlSessionFactory sqlSessionFactory = SessionFactoryCache.factoryMap.get(uri);
         if (sqlSessionFactory == null) {
@@ -51,18 +72,54 @@ public class BaseController {
         //通过SqlsessionFactory打开一个数据库会话
         try (SqlSession session = sqlSessionFactory.openSession()) {
             BaseMapper mapper = session.getMapper(BaseMapper.class);
+
+            //添加 INSERT UPDATE DELETE 相关语句
+            if(uri.contains("insert") || uri.contains("update") || uri.contains("delete") ){
+                int result = 0;
+
+                if(uri.contains("insert")){
+                    if(uri.contains("insertCategoryConfig")){
+                        if(!mapper.execute(paramMap).toString().equals("[]")){
+                            return  AjaxResult.NOT_MODIFIED(new ArrayList<>());
+                        }
+                    }
+                    result = mapper.insertExecute(paramMap);
+                }
+
+                if(uri.contains("update")){
+                    result = mapper.updateExecute(paramMap);
+                }
+
+                if(uri.contains("delete")){
+                    result = mapper.deleteExecute(paramMap);
+                }
+                session.commit();//手动提交事务
+
+                return AjaxResult.success(result);
+            }
+
+
+
             IPage page = buildIPage(paramMap);
             if (page != null) {
                 paramMap.put("_PAGE_", page);
             }
+
             List<Map<String, Object>> result =  mapper.execute(paramMap);
-            if(result.toString().equals("[null]")){
-                return AjaxResult.success(new ArrayList<>());
+
+//            if(result.toString().equals("[null]")){
+//                return AjaxResult.no_content(new ArrayList<>());
+//            }
+
+            if(result.toString().equals("[]")){
+                return AjaxResult.no_content(new ArrayList<>());
             }
+
             if (page != null) {
                 page.setRecords(result);
                 return AjaxResult.success(page);
             }
+
             return AjaxResult.success(result);
         }
     }
@@ -93,4 +150,6 @@ public class BaseController {
         loadMapperData.load(-1);
         return true;
     }
+
+
 }
